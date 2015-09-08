d4c6949d3372fdc2d7ef5518db9fe21c3b496bb2
sakah <h0.sakamoto@gmail.com>
2015-09-08 15:07:10 +0900

    update iev=2

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@
 comment="$1"
 echo \"$comment\"
 git add .; git commit -m "$comment"; git push origin master
-#expect "Enter passphrase for key '/home/had/hideyuki/.ssh/id_rsa':"
-#send "Pinkyslot123\r"
-#interact
+expect "Enter passphrase for key '/home/had/hideyuki/.ssh/id_rsa': "
+send "Pinkyslot123\r"
+interact
