c70428d4161c720595437411ab9b501af8638d45
sakah <h0.sakamoto@gmail.com>
2015-09-08 13:46:19 +0900

    Initial commit

---
+++ main.cc
@@ -0,0 +1,198 @@
+/*
+   layer   1      0     G      +       51.1673      51.4000      147.6046     396         12          190.400        0.2327      66.106       0.0000
+   layer   2      1     F      -       52.0358      52.2000      147.8867     396        -12         -190.400        0.1642     -67.004       15.867
+   layer   3      1     S      -       52.8333      53.0000      148.1688     396        -12         -190.400        0.1667     -67.899       15.867
+   layer   4      2     F      +       53.5705      53.8000      148.4509     408         12          184.800        0.2295      66.778       15.400
+   layer   5      2     S      +       54.3671      54.6000      148.7331     408         12          184.800        0.2329      67.640       15.400
+   layer   6      3     F      -       55.1770      55.4000      149.0152     420        -12         -179.520        0.2230     -66.553       0.0000
+   layer   7      3     S      -       55.9738      56.2000      149.2973     420        -12         -179.520        0.2262     -67.384       0.0000
+   layer   8      4     F      +       56.7831      57.0000      149.5794     432         12          174.533        0.2169      66.327       14.544
+   layer   9      4     S      +       57.5801      57.8000      149.8616     432         12          174.533        0.2199      67.129       14.544
+   layer   10     5     F      -       58.3889      58.6000      150.1437     444        -12         -169.816        0.2111     -66.102       0.0000
+   layer   11     5     S      -       59.1860      59.4000      150.4258     444        -12         -169.816        0.2140     -66.876       0.0000
+   layer   12     6     F      +       59.9944      60.2000      150.7079     456         12          165.347        0.2056      65.877       0.0000
+   layer   13     6     S      +       60.7917      61.0000      150.9900     456         12          165.347        0.2083      66.625       0.0000
+   layer   14     7     F      -       61.5996      61.8000      151.2722     468        -12         -161.107        0.2004     -65.652       13.426
+   layer   15     7     S      -       62.3970      62.6000      151.5543     468        -12         -161.107        0.2030     -66.376       13.426
+   layer   16     8     F      +       63.2046      63.4000      151.8364     480         12          157.080        0.1954      65.428       0.0000
+   layer   17     8     S      +       64.0021      64.2000      152.1185     480         12          157.080        0.1979      66.129       0.0000
+   layer   18     9     F      -       64.8093      65.0000      152.4007     492        -12         -153.248        0.1907     -65.205       12.771
+   layer   19     9     S      -       65.6069      65.8000      152.6828     492        -12         -153.248        0.1931     -65.884       12.771
+   layer   20     10    F      +       66.4138      66.6000      152.9649     504         12          149.600        0.1862      64.982       0.0000
+   layer   21     10    S      +       67.2115      67.4000      153.2470     504         12          149.600        0.1885      65.640       0.0000
+   layer   22     11    F      -       68.0181      68.2000      153.5292     516        -12         -146.121        0.1819     -64.761       12.177
+   layer   23     11    S      -       68.8159      69.0000      153.8113     516        -12         -146.121        0.1841     -65.398       12.177
+   layer   24     12    F      +       69.6222      69.8000      154.0934     528         12          142.800        0.1778      64.540       0.0000
+   layer   25     12    S      +       70.4201      70.6000      154.3755     528         12          142.800        0.1799      65.158       0.0000
+   layer   26     13    F      -       71.2261      71.4000      154.6576     540        -12         -139.626        0.1739     -64.319       11.636
+   layer   27     13    S      -       72.0241      72.2000      154.9398     540        -12         -139.626        0.1759     -64.920       11.636
+   layer   28     14    F      +       72.8298      73.0000      155.2219     552         12          136.591        0.1702      64.100       0.0000
+   layer   29     14    S      +       73.6280      73.8000      155.5040     552         12          136.591        0.1720      64.683       0.0000
+   layer   30     15    F      -       74.3733      74.6000      155.7861     564        -14         -155.966        0.2267     -74.472       0.0000
+   layer   31     15    S      -       75.1709      75.4000      156.0683     564        -14         -155.966        0.2291     -75.132       0.0000
+   layer   32     16    F      +       75.9780      76.2000      156.3504     576         14          152.716        0.2220      74.220       0.0000
+   layer   33     16    S      +       76.7756      77.0000      156.6325     576         14          152.716        0.2244      74.862       0.0000
+   layer   34     17    F      -       77.5825      77.8000      156.9146     588        -14         -149.600        0.2175     -73.969       10.686
+   layer   35     17    S      -       78.3802      78.6000      157.1968     588        -14         -149.600        0.2198     -74.593       10.686
+   layer   36     18    F      +       79.1868      79.4000      157.4789     600         14          146.608        0.2132      73.719       0.0000
+   layer   37     18    S      +       79.9846      80.2000      157.7610     600         14          146.608        0.2154      74.326       0.0000
+   layer   38     19    F      -       80.7909      81.0000      158.0431     612        -14         -143.733        0.2091     -73.470       10.267
+   layer   39     19    G      -       81.5889      81.8000      158.3252     612        -14         -143.733        0.2111     -74.061       10.267
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+
+#include "TCanvas.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "TMarker.h"
+#include "InputROOT.h"
+#include "TGraphErrors.h"
+#include "TEllipse.h"
+
+
+struct Circle
+{
+   int nhits;
+   double xhits[1000];
+   double yhits[1000];
+   double radius[20];
+   Circle()
+   {
+      nhits = 0;
+
+      int n = 0;
+      radius[n++] = 51.4000;
+      radius[n++] = 53.0000;
+      radius[n++] = 54.6000;
+      radius[n++] = 56.2000;
+      radius[n++] = 57.8000;
+      radius[n++] = 59.4000;
+      radius[n++] = 61.0000;
+      radius[n++] = 62.6000;
+      radius[n++] = 64.2000;
+      radius[n++] = 65.8000;
+      radius[n++] = 67.4000;
+      radius[n++] = 69.0000;
+      radius[n++] = 70.6000;
+      radius[n++] = 72.2000;
+      radius[n++] = 73.8000;
+      radius[n++] = 75.4000;
+      radius[n++] = 77.0000;
+      radius[n++] = 78.6000;
+      radius[n++] = 80.2000;
+      radius[n++] = 81.8000;
+   };
+   void fit()
+   {
+   };
+   void add_hit(double x, double y)
+   {
+      xhits[nhits] = x;
+      yhits[nhits] = y;
+      nhits++;
+   };
+   void draw()
+   {
+      draw_radius();
+
+      for (int ihit=0; ihit<nhits; ihit++) {
+         TMarker* m = new TMarker(xhits[ihit], yhits[ihit], 8);
+         m->Draw();
+      }
+   };
+   void draw_radius()
+   {
+      for (int ilayer=0; ilayer<20; ilayer++) {
+         if (ilayer%2==0) continue;
+         TEllipse* e = new TEllipse(0,0,radius[ilayer]);
+         e->SetFillStyle(0);
+         e->SetLineWidth(1);
+         e->SetLineStyle(1);
+         e->Draw();
+      }
+   };
+};
+
+int main(int argc, char** argv)
+{
+   if (argc != 8) {
+      fprintf(stderr,"Usage %s <input.root> <wire_config.txt> <sample_type> <t2r_type> <rdrift_err_cm> <posSmear_cm> <momSmear_percent>\n", argv[0]);
+      return -1;
+   }
+   char* input_root = argv[1];
+   char* wire_config_txt = argv[2];
+   char* sample_type = argv[3];
+   char* t2r_type = argv[4];
+   double rdrift_err_cm = atof(argv[5]);
+   double posSmear_cm = atof(argv[6]);
+   double momSmear_percent = atof(argv[7]);
+
+   InputROOT inROOT(input_root, wire_config_txt, sample_type, t2r_type, rdrift_err_cm, posSmear_cm, momSmear_percent);
+   //int total = inROOT.getEntries();
+   //int total = 100;
+
+   double w_x1;
+   double w_y1;
+   double w_z1;
+   double w_x2;
+   double w_y2;
+   double w_z2;
+   double w_x;
+   double w_y;
+   double w_z;
+
+   TVector3 mcPos;
+   TVector3 mcMom;
+
+
+   TH2F* h1 = new TH2F("h1", "", 200, -100, 100, 200, -150, 150);
+
+   char title[12];
+   for (int iev=0; iev<100; iev++) {
+
+      inROOT.getEntry(iev);
+      bool directHit = inROOT.InDirectHitAtTriggerCounter();
+      if (!directHit) continue;
+
+      int numHits = inROOT.getNumHits();
+      if (numHits==0) continue;
+
+      printf("iev %d numHits %d\n", iev, numHits );
+
+      struct Circle circ1;
+      struct Circle circ2;
+
+
+      double zpos = -1;
+      int icell1 = -1;
+      int icell2 = -1;
+      for (int ihit=0; ihit<numHits; ihit++) {
+         int ilayer = inROOT.getIlayer(ihit);
+         int icell = inROOT.getIcell(ihit);
+         int iturn = inROOT.getIturn(ihit);
+         //if (iturn!=0) continue; 
+         printf("ilayer %d icell %d iturn %d\n", ilayer, icell, iturn);
+
+         inROOT.getPosMom(ihit, mcPos, mcMom);
+         inROOT.getWirePosAtEndPlates(ihit, w_x1, w_y1, w_z1, w_x2, w_y2, w_z2);
+         inROOT.getWirePosAtHitPoint(ihit, w_x, w_y, w_z);
+
+         if (ilayer%2==1) circ1.add_hit(w_x1, w_y1);
+         if (ilayer%2==0) circ2.add_hit(w_x1, w_y1);
+      }
+
+      circ1.fit();
+      circ2.fit();
+
+      TCanvas* c1 = new TCanvas("c1");
+      c1->Divide(2,1);
+      c1->cd(1); circ1.draw();
+      c1->cd(2); circ2.draw();
+      c1->Print(Form("pdf/%05d.pdf", iev));
+   }
+
+   return 0;
+}
