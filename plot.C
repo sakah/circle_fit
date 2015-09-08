e36fc56b2dca912804bd4008f2a32ba7f9ed746a
sakah <h0.sakamoto@gmail.com>
2015-09-08 22:40:49 +0900

    Run 20k events

--- plot.C
+++ plot.C
@@ -1,5 +1,5 @@
 {
    TTree t;
    t.SetMarkerStyle(20);
-   t.ReadFile("debug.txt","iev/I:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit");
+   t.ReadFile("debug.txt","iev/I:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit:chi2");
 }
