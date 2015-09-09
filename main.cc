ddb05844e11f8db17ddb87d2e6f2c7dde208e98b
sakah <h0.sakamoto@gmail.com>
2015-09-09 16:57:19 +0900

    Test 30 events

--- main.cc
+++ main.cc
@@ -735,10 +735,10 @@ int main(int argc, char** argv)
    //int iev1=8, iev2=9;
    //int iev1=10, iev2=11;
    //int iev1=11, iev2=12;
-   int iev1=13, iev2=14;
+   //int iev1=13, iev2=14;
    //int iev1=14, iev2=15;
    //int iev1=0, iev2=3;
-   //int iev1=0, iev2=30;
+   int iev1=0, iev2=30;
    //int iev1=0, iev2=2000;
    for (int iev=iev1; iev<iev2; iev++) { 
       fprintf(stderr,"iev %d\n", iev);
