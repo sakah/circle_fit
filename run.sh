4de9e0d992a897a63d268f0d0bd7aaddc8266046
sakah <h0.sakamoto@gmail.com>
2015-09-10 23:59:13 +0900

    change path name in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./main config1.txt output/run1/ 10 10 yes > /output/run1/log.txt & # single-0%-0.02-threshold
-./main config2.txt output/run2/ 10 10 yes > /output/run2/log.txt & # multi -0%-0.02-threshold
-./main config3.txt output/run3/ 10 10 yes > /output/run3/log.txt & # single-10%-0.02-threshold
-./main config4.txt output/run4/ 10 10 yes > /output/run4/log.txt & # multi -10%-0.02-threshold
+./main config1.txt output/run1/ 10 10 yes > output/run1/log.txt & # single-0%-0.02-threshold
+./main config2.txt output/run2/ 10 10 yes > output/run2/log.txt & # multi -0%-0.02-threshold
+./main config3.txt output/run3/ 10 10 yes > output/run3/log.txt & # single-10%-0.02-threshold
+./main config4.txt output/run4/ 10 10 yes > output/run4/log.txt & # multi -10%-0.02-threshold
