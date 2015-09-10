166e3fa8adaaea4cb4be1cb2cd1b48c3446ad11e
sakah <h0.sakamoto@gmail.com>
2015-09-11 08:29:41 +0900

    Run diff_thre=0.001

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ run()
    ./main $config $output_dir $iev1 $iev2 $mkpdf > $output_dir/log.txt 2> $output_dir/err &
 }
 
+# thre=0.02
 #run config1.txt output/run1/ 10 10 yes # single-0%-0.02-threshold
 #run config2.txt output/run2/ 10 10 yes # multi -0%-0.02-threshold
 #run config3.txt output/run3/ 10 10 yes # single-10%-0.02-threshold
@@ -23,6 +24,7 @@ run()
 #run config3.txt output/run3/ 0 1999 no # single-10%-0.02-threshold
 #run config4.txt output/run4/ 0 1999 no # multi -10%-0.02-threshold
 
+# thre=0.01
 #run config1.txt output/run5/ 10 10 yes # single-0%-0.01-threshold
 #run config2.txt output/run6/ 10 10 yes # multi -0%-0.01-threshold
 #run config3.txt output/run7/ 10 10 yes # single-10%-0.01-threshold
@@ -33,12 +35,24 @@ run()
 #run config3.txt output/run7/ 0 1999 no # single-10%-0.01-threshold
 #run config4.txt output/run8/ 0 1999 no # multi -10%-0.01-threshold
 
+# thre=0.005
 #run config1.txt output/run9/ 10 10 yes # single-0%-0.005-threshold
 #run config2.txt output/run10/ 10 10 yes # multi -0%-0.005-threshold
 #run config3.txt output/run11/ 10 10 yes # single-10%-0.005-threshold
 #run config4.txt output/run12/ 10 10 yes # multi -10%-0.005-threshold
 
-run config1.txt output/run9/  0 1999 no # single-0%-0.005-threshold
-run config2.txt output/run10/ 0 1999 no # multi -0%-0.005-threshold
-run config3.txt output/run11/ 0 1999 no # single-10%-0.005-threshold
-run config4.txt output/run12/ 0 1999 no # multi -10%-0.005-threshold
+#run config1.txt output/run9/  0 1999 no # single-0%-0.005-threshold
+#run config2.txt output/run10/ 0 1999 no # multi -0%-0.005-threshold
+#run config3.txt output/run11/ 0 1999 no # single-10%-0.005-threshold
+#run config4.txt output/run12/ 0 1999 no # multi -10%-0.005-threshold
+
+# thre=0.001
+#run config1.txt output/run13/ 10 10 yes # single-0%-0.001-threshold
+#run config2.txt output/run14/ 10 10 yes # multi -0%-0.001-threshold
+#run config3.txt output/run15/ 10 10 yes # single-10%-0.001-threshold
+#run config4.txt output/run16/ 10 10 yes # multi -10%-0.001-threshold
+
+run config1.txt output/run13/ 0 1999 no # single-0%-0.001-threshold
+run config2.txt output/run14/ 0 1999 no # multi -0%-0.001-threshold
+run config3.txt output/run15/ 0 1999 no # single-10%-0.001-threshold
+run config4.txt output/run16/ 0 1999 no # multi -10%-0.001-threshold
