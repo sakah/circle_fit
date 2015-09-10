1fe30030e86e8e7690c3a9237e35d0aea418f06f
sakah <h0.sakamoto@gmail.com>
2015-09-10 13:54:15 +0900

    Test iev329, Update plot.C

--- plot.C
+++ plot.C
@@ -1,5 +1,5 @@
 {
    TTree t;
    t.SetMarkerStyle(20);
-   t.ReadFile("debug.txt","iev/I:sig1:sig1in:sig1out:sig2:sig2in:sig2out:num:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit:chi2");
+   t.ReadFile("debug.txt","iev/I:sig1:sig1in:sig1out:in1:in1sig:in1noise:sig2:sig2in:sig2out:in2:in2sig:in2noise:num:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit:chi2");
 }
