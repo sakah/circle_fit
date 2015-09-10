dc37d8dd74145d4af82f42bafe90c8a54a1fd97e
sakah <h0.sakamoto@gmail.com>
2015-09-11 00:06:53 +0900

    Run 4 configs

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-./main config1.txt output/run1/ 10 10 yes > output/run1/log.txt & # single-0%-0.02-threshold
-./main config2.txt output/run2/ 10 10 yes > output/run2/log.txt & # multi -0%-0.02-threshold
-./main config3.txt output/run3/ 10 10 yes > output/run3/log.txt & # single-10%-0.02-threshold
-./main config4.txt output/run4/ 10 10 yes > output/run4/log.txt & # multi -10%-0.02-threshold
+run()
+{
+   config=$1
+   output_dir=$2
+   iev1=$3
+   iev2=$4
+   mkpdf=$5
+   [ ! -e $output_dir ] && mkdir $output_dir
+   echo "./main $config $output_dir $iev1 $iev2 $mkpdf > $output_dir/log.txt 2> $output_dir/err &"
+   ./main $config $output_dir $iev1 $iev2 $mkpdf > $output_dir/log.txt 2> $output_dir/err &
+}
+
+run config1.txt output/run1/ 10 10 yes # single-0%-0.02-threshold
+run config2.txt output/run2/ 10 10 yes # multi -0%-0.02-threshold
+run config3.txt output/run3/ 10 10 yes # single-10%-0.02-threshold
+run config4.txt output/run4/ 10 10 yes # multi -10%-0.02-threshold
