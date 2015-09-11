c8797469aeabc1ff2c8d3969f12e74f30e8ac987
sakah <h0.sakamoto@gmail.com>
2015-09-11 10:08:04 +0900

    update macro

--- macro/plot.C
+++ macro/plot.C
@@ -6,42 +6,84 @@
 class Run
 {
    public:
-      Run(int id, double diff_thre)
+      Run(int irun, char* setup)
       {
-         plot_id = id;
-         diff_threshold = diff_thre;
+         read_setup(irun, setup);
+
+         char* data = Form("../output/run%d/output.txt", irun);
+         read(data);
       };
-      void add_data(int id, double max_chi2, char* data, char* title)
+
+      TTree *t;
+      double max_chi2;
+      char title[32];
+      char turn_type[32];
+      char noise_occupancy[32];
+      double diff_threshold;
+
+   private:
+      void read(char* txt)
       {
-         if (id==0) { max_chi2_1 = max_chi2; t1 = read(data); sprintf(title1, "%s/%5.4lf", title, diff_threshold); }
-         if (id==1) { max_chi2_2 = max_chi2; t2 = read(data); sprintf(title2, "%s/%5.4lf", title, diff_threshold); }
-         if (id==2) { max_chi2_3 = max_chi2; t3 = read(data); sprintf(title3, "%s/%5.4lf", title, diff_threshold); }
-         if (id==3) { max_chi2_4 = max_chi2; t4 = read(data); sprintf(title4, "%s/%5.4lf", title, diff_threshold); }
+         t = new TTree("t","t");
+         t->SetMarkerStyle(20);
+         t->ReadFile(txt,"iev/I:sig1:sig1in:sig1out:in1:in1sig:in1noise:sig2:sig2in:sig2out:in2:in2sig:in2noise:num:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit:chi2");
       };
-      void plot()
+
+      void read_setup(int irun, char* txt)
+      {
+         FILE* fp = fopen(txt,"r");
+         if (fp==NULL) {
+            fprintf(stderr,"ERROR: faield to open setup file %s\n", txt);
+            return;
+         }
+         char line[128];
+         int run_num;
+         char type[32];
+         char noise[32];
+         double threshold;
+         double maxchi2;
+
+         while(fgets(line,sizeof(line),fp)) {
+            sscanf(line, "%d %s %s %lf %lf",&run_num,type,noise,&threshold,&maxchi2);
+            if (irun==run_num) {
+               strcpy(turn_type,type);
+               strcpy(noise_occupancy,noise);
+               diff_threshold = threshold;
+               max_chi2 = maxchi2;
+
+               sprintf(title, "Run%d/%s/%s/%5.4f", run_num,type,noise,threshold);
+            }
+         }
+         fclose(fp);
+      };
+};
+struct Plot
+{
+   public:
+      void plot(int plot_id, Run& run1, Run& run2, Run& run3, Run& run4)
       {
          gStyle->SetOptStat(111111);
          gStyle->SetOptFit();
 
-         TH1F* h11 = get_chi2(t1, "t1", title1, max_chi2_1);
-         TH1F* h21 = get_chi2(t2, "t2", title2, max_chi2_2);
-         TH1F* h31 = get_chi2(t3, "t3", title3, max_chi2_3);
-         TH1F* h41 = get_chi2(t4, "t4", title4, max_chi2_4);
+         TH1F* h11 = get_chi2(run1.t, "t1", run1.title, run1.max_chi2);
+         TH1F* h21 = get_chi2(run2.t, "t2", run2.title, run2.max_chi2);
+         TH1F* h31 = get_chi2(run3.t, "t3", run3.title, run3.max_chi2);
+         TH1F* h41 = get_chi2(run4.t, "t4", run4.title, run4.max_chi2);
 
-         TH1F* h12 = get_ptres(t1, "t1", title1);
-         TH1F* h22 = get_ptres(t2, "t2", title2);
-         TH1F* h32 = get_ptres(t3, "t3", title3);
-         TH1F* h42 = get_ptres(t4, "t4", title4);
+         TH1F* h12 = get_ptres(run1.t, "t1", run1.title);
+         TH1F* h22 = get_ptres(run2.t, "t2", run2.title);
+         TH1F* h32 = get_ptres(run3.t, "t3", run3.title);
+         TH1F* h42 = get_ptres(run4.t, "t4", run4.title);
 
-         TH1F* h13 = get_pzres(t1, "t1", title1);
-         TH1F* h23 = get_pzres(t2, "t2", title2);
-         TH1F* h33 = get_pzres(t3, "t3", title3);
-         TH1F* h43 = get_pzres(t4, "t4", title4);
+         TH1F* h13 = get_pzres(run1.t, "t1", run1.title);
+         TH1F* h23 = get_pzres(run2.t, "t2", run2.title);
+         TH1F* h33 = get_pzres(run3.t, "t3", run3.title);
+         TH1F* h43 = get_pzres(run4.t, "t4", run4.title);
 
-         TH1F* h14 = get_z(t1, "t1", title1);
-         TH1F* h24 = get_z(t2, "t2", title2);
-         TH1F* h34 = get_z(t3, "t3", title3);
-         TH1F* h44 = get_z(t4, "t4", title4);
+         TH1F* h14 = get_z(run1.t, "t1", run1.title);
+         TH1F* h24 = get_z(run2.t, "t2", run2.title);
+         TH1F* h34 = get_z(run3.t, "t3", run3.title);
+         TH1F* h44 = get_z(run4.t, "t4", run4.title);
 
          TCanvas* c1 = new TCanvas("c1", "chi2");
          c1->Divide(2,2);
@@ -76,32 +118,8 @@ class Run
          c3->Print(Form("pdf/pzres/c%d.pdf", plot_id));
          c4->Print(Form("pdf/zpos/c%d.pdf",  plot_id));
       };
+
    private:
-      int plot_id;
-      double diff_threshold;
-      TTree *t1;
-      TTree *t2;
-      TTree *t3;
-      TTree *t4;
-      double max_chi2_1;
-      double max_chi2_2;
-      double max_chi2_3;
-      double max_chi2_4;
-      char title1[32];
-      char title2[32];
-      char title3[32];
-      char title4[32];
-      char pdf1[32];
-      char pdf2[32];
-      char pdf3[32];
-      char pdf4[32];
-      TTree* read(char* txt)
-      {
-         TTree *t =new TTree("t","t");
-         t->SetMarkerStyle(20);
-         t->ReadFile(txt,"iev/I:sig1:sig1in:sig1out:in1:in1sig:in1noise:sig2:sig2in:sig2out:in2:in2sig:in2noise:num:dr/D:deg:zhit:zfit:pthit:ptfit:pzhit:pzfit:chi2");
-         return t;
-      };
       TH1F* get_chi2(TTree* t, char* hname, char* htitle, double max_chi2)
       {
          char* hname1 = Form("h1-%s", hname);
@@ -135,46 +153,43 @@ class Run
          return h1;
       };
 };
-void run1()
+
+void thre1()
 {
-   Run run(1, 0.02);
-   run.add_data(0, 5,   "../output/run1/output.txt", "Run1/Single/0%");
-   run.add_data(1, 5,   "../output/run2/output.txt", "Run2/Multi/0%");
-   run.add_data(2, 150, "../output/run3/output.txt", "Run3/Single/10%");
-   run.add_data(3, 150, "../output/run4/output.txt", "Run4/Multi/10%");
-   run.plot();
+   Run run1(1, "setup.txt");
+   Run run2(2, "setup.txt");
+   Run run3(3, "setup.txt");
+   Run run4(4, "setup.txt");
+   Plot plot; plot.plot(1,run1,run2,run3,run4);
 }
-void run2()
+void thre2()
 {
-   Run run(2, 0.01);
-   run.add_data(0, 5,   "../output/run5/output.txt", "Run5/Single/0%");
-   run.add_data(1, 5,   "../output/run6/output.txt", "Run6/Multi/0%");
-   run.add_data(2, 100, "../output/run7/output.txt", "Run7/Single/10%");
-   run.add_data(3, 100, "../output/run8/output.txt", "Run8/Multi/10%");
-   run.plot();
+   Run run1(5, "setup.txt");
+   Run run2(6, "setup.txt");
+   Run run3(7, "setup.txt");
+   Run run4(8, "setup.txt");
+   Plot plot; plot.plot(2,run1,run2,run3,run4);
 }
-void run3()
+void thre3()
 {
-   Run run(3, 0.005);
-   run.add_data(0, 5, "../output/run9/output.txt",   "Run9/Single/0%");
-   run.add_data(1, 5, "../output/run10/output.txt",  "Run10/Multi/0%");
-   run.add_data(2, 20, "../output/run11/output.txt", "Run11/Single/10%");
-   run.add_data(3, 20, "../output/run12/output.txt", "Run12/Multi/10%");
-   run.plot();
+   Run run1(9,  "setup.txt");
+   Run run2(10, "setup.txt");
+   Run run3(11, "setup.txt");
+   Run run4(12, "setup.txt");
+   Plot plot; plot.plot(3,run1,run2,run3,run4);
 }
-void run4()
+void thre4()
 {
-   Run run(4, 0.001);
-   run.add_data(0, 5, "../output/run13/output.txt", "Run13/Single/0%");
-   run.add_data(1, 5, "../output/run14/output.txt", "Run14/Multi/0%");
-   run.add_data(2, 5, "../output/run15/output.txt", "Run15/Single/10%");
-   run.add_data(3, 5, "../output/run16/output.txt", "Run16/Multi/10%");
-   run.plot();
+   Run run1(13, "setup.txt");
+   Run run2(14, "setup.txt");
+   Run run3(15, "setup.txt");
+   Run run4(16, "setup.txt");
+   Plot plot; plot.plot(4,run1,run2,run3,run4);
 }
 void all()
 {
-   run1();
-   run2();
-   run3();
-   run4();
+   thre1();
+   thre2();
+   thre3();
+   thre4();
 }
